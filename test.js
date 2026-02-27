859eca1546f389f4331dd102d3c664582029c602
lctnov <chauthoi1010@gmail.com>
2026-02-27 10:49:32 +0700

    commit test

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-console.log("Starting test...");
+console.log("\nTesting CI/CD pipeline...");
 setTimeout(() => {
 	console.log("Waiting for 3 seconds...");
 }, 3000);
-console.log("Testing complete CI/CD!!!");
+console.log("Testing complete!!!");
 
